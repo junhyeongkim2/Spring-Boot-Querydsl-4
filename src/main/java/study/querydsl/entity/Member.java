bc41c3422d324fc1d0494bde36bbfd41b3155338
jun <ggprgrkjh@naver.com>
2023-06-29 19:05:32 +0900

    feat: Team Entity

--- src/main/java/study/querydsl/entity/Member.java
+++ src/main/java/study/querydsl/entity/Member.java
@@ -1,16 +1,13 @@
 package study.querydsl.entity;
 
-import lombok.Generated;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
 public class Member {
 
     @Id @GeneratedValue
@@ -19,4 +16,29 @@ public class Member {
     private String username;
     private int age;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team!=null){
+            changeTeam(team);
+        }
+    }
+    private void changeTeam(Team team) {
+        this.team=team;
+        team.getMembers().add(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                ", team=" + team +
+                '}';
+    }
 }
