609e3703d9ff49d64fcef38cba825c6df2c8d26c
jun <ggprgrkjh@naver.com>
2023-07-01 17:45:15 +0900

    feat: theta_join test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -259,6 +259,25 @@ public class QuerydslBasicTest {
     }
 
 
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA","teamB");
+
+    }
+
+
 
 
 
