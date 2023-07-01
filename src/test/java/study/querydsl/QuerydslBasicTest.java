62448fa16df1b48b609cf2a32ec9903d7c17e539
jun <ggprgrkjh@naver.com>
2023-07-01 17:05:49 +0900

    feat: paing2 Test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -173,10 +173,22 @@ public class QuerydslBasicTest {
                 .fetch();
 
         assertThat(result.size()).isEqualTo(2);
-
     }
 
+    @Test
+    public void paging2(){
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
 
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 
 
 
