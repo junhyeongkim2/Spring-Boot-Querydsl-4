bcfd59b2a4ec6dc5588b9bf5f4125956564494aa
jun <ggprgrkjh@naver.com>
2023-07-02 22:14:21 +0900

    feat: fetchJoinNo test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -17,7 +17,9 @@ import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
@@ -316,13 +318,31 @@ public class QuerydslBasicTest {
         List<Tuple> result = queryFactory
                 .select(member,team)
                 .from(member)
-                .leftJoin(team)
+                .join(team)
                 .on(member.username.eq(team.name))
                 .fetch();
 
         for (Tuple tuple : result) {
             System.out.println("tuple = " + tuple);
         }
+    }
+
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+    @Test
+    public void fetchJoinNo(){
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+
 
     }
 
