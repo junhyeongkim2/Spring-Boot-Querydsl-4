384aecf80fd3b7ee39296c5f5ca33857c3cb5a88
jun <ggprgrkjh@naver.com>
2023-07-02 22:25:43 +0900

    feat: subQueryIn test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -411,7 +411,29 @@ public class QuerydslBasicTest {
 
     }
 
+    /**
+     * 나이가 평균 이상인 회원
+     */
 
+    @Test
+    public void subQueryIn(){
+
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.in(
+                        JPAExpressions
+                                .select(memberSub.age)
+                                .from(memberSub)
+                                .where(memberSub.age.gt(10))
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age")
+                .containsExactly(20,20);
+
+    }
 
 
 
