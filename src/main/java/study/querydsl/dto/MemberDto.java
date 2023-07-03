f030957176f7aed4ecf25b49054123efb27ccd12
jun <ggprgrkjh@naver.com>
2023-07-03 23:50:16 +0900

    feat: findDtoByConstructor test

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -15,4 +15,19 @@ public class MemberDto {
         this.age = age;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
