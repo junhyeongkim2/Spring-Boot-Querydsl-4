d7f73af10c90b29b5b473c1163be9c69918d9d11
jun <ggprgrkjh@naver.com>
2023-07-03 23:35:29 +0900

    feat: simpleProjection test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -511,6 +511,19 @@ public class QuerydslBasicTest {
             System.out.println("string = " + st);
         }
     }
+    
+    
+    @Test
+    public void simpleProjection(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 
 
 
