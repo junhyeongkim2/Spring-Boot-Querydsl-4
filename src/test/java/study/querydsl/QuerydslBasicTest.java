13cb0dc9e10f1e2dd0f0776beefd5fac8361af20
jun <ggprgrkjh@naver.com>
2023-07-04 16:37:23 +0900

    feat: dynamicQuery_BooleanBuilder test

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
@@ -632,16 +633,39 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result){
             System.out.println("memberDto = " + memberDto);
         }
-
-
     }
 
 
+    @Test
+    public void dynamicQuery_BooleanBuilder(){
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam,ageParam);
+        assertThat(result.size()).isEqualTo(1);
+
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if(usernameCond != null){
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if(ageCond != null){
+            builder.and(member.age.eq(ageCond));
+        }
 
 
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
 
 
-
+    }
 
 
 }
